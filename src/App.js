f1c3facf425c1c428c959da83631242261231207
akamaliev <72889027+akamaliev@users.noreply.github.com>
2022-06-16 05:59:58 +0300

    continue

--- src/App.js
+++ src/App.js
@@ -8,21 +8,43 @@ function App() {
   const [elements, setElements] = useState([]);
   const refApp = useRef(null);
 
+  let addElem = (previousElemId, elemId, innerHtml) => {
+    setElements((elements) => {
+      return elements.reduce((accum, current) => {
+        accum.push(current);
+        if (current.props.id === previousElemId) {
+          accum.push(
+            <Simple key={elemId} id={elemId}>
+              {innerHtml}
+            </Simple>
+          );
+        }
+        return accum;
+      }, []);
+    });
+  };
+
+
   useEffect(() => {
-
     const appElem = refApp.current;
-
 
     let observer = new MutationObserver((mutationRecords) => {
       console.log(mutationRecords);
-      if(mutationRecords[0].addedNodes.length > 0) {
+      if (mutationRecords[0].addedNodes.length > 0) {
+        // let addedElem = mutationRecords[0].addedNodes[0];
+        // console.log(addedElem); // console.log(изменения)
+        // console.log(addedElem.innerHTML);
+        // addedElem.innerHTML = addedElem.innerText;
+        // console.log(addedElem.innerHTML);
+        let elemId = generateId();
+        let mut = mutationRecords[0];
+        addElem(mut.previousSibling.id, elemId, mut.addedNodes[0].innerHTML);
 
-        let addedElem = mutationRecords[0].addedNodes[0];
-        console.log(addedElem); // console.log(изменения)
-        console.log(addedElem.innerHTML);
-        addedElem.innerHTML = addedElem.innerText;
-        console.log(addedElem.innerHTML);
-        addedElem.id = generateId();
+        console.log(
+          mut.previousSibling.id,
+          elemId,
+          mut.addedNodes[0].innerHTML
+        );
 
         // let observer_child = new MutationObserver((mutRecords) => {
         //   console.log('Child mutation:');
@@ -39,16 +61,17 @@ function App() {
         // setElements((elements) => {
         //   return [...elements];
         // });
-      };
+      }
     });
     observer.observe(appElem, {
       childList: true, // наблюдать за непосредственными детьми
       // subtree: true, // и более глубокими потомками
       characterDataOldValue: true, // передавать старое значение в колбэк
     });
-
-    setElements([<Simple key={1} />]);
-  }, [setElements]);
+    let elemId = generateId();
+    setElements([<Simple key={elemId} id={elemId}>Hello</Simple>]);
+    // addElem(null, generateId(), "<b>Zirnyi</b> <i>cursive</i>");
+  }, []);
 
   console.log('render');
 
