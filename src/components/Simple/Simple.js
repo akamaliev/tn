d0631f881e813d434e2935ecf08c8973331ea3f3
akamaliev <72889027+akamaliev@users.noreply.github.com>
2022-06-16 06:00:47 +0300

    simple component

--- src/components/Simple/Simple.js
+++ src/components/Simple/Simple.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-export default function Simple() {
+export default function Simple({ id, children }) {
+  const refSimple = useRef(null);
+
+  useEffect(() => {
+    refSimple.current.focus();
+  }, []);
+
   return (
-    <h1 id="dddd123" className='blue'>Simple</h1>
-  )
+    <h1
+      ref={refSimple}
+      id={id}
+      className="blue"
+      contentEditable
+      suppressContentEditableWarning
+    >
+      {children}
+    </h1>
+  );
 }
